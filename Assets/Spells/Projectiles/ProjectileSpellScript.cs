ee6f18009f173db2a05c7134862d584cf83948b9
Kyle Kanaly <kkanaly@ewa.com>
2022-07-20 15:05:45 -0400

    Starting spell logic

---
+++ Assets/Spells/Projectiles/ProjectileSpellScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpellScript : MonoBehaviour
+{
+	
+	//public RigidBody2D RB
+
+	public void OnAwake()
+	{
+		//RB.velocity;
+	}
+
+}
