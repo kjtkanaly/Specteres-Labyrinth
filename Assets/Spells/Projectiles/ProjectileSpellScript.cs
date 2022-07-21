1aeeab3ea2ac84be45f97ca3b7f13ec8dffbc282
kjtkanaly <kdude2525@gmail.com>
2022-07-20 22:51:19 -0400

    Projerctile Stuff

--- Assets/Spells/Projectiles/ProjectileSpellScript.cs
+++ Assets/Spells/Projectiles/ProjectileSpellScript.cs
@@ -4,12 +4,43 @@ using UnityEngine;
 
 public class ProjectileSpellScript : MonoBehaviour
 {
-	
-	//public RigidBody2D RB
+    public Rigidbody2D RB;
+    public GameObject MC, Reticle, Weapon;
+    private Animator Anime;
 
-	public void OnAwake()
-	{
-		//RB.velocity;
-	}
+    private Vector2 reticlePosRelativeToHilt;
+    public float bulletSpeed = 40f;
+    public float bulletHitForce = 20f;
+
+
+    private void Awake()
+    {
+        MC = GameObject.FindGameObjectWithTag("Player");
+        Weapon = GameObject.FindGameObjectWithTag("Weapon");
+
+        reticlePosRelativeToHilt = (Camera.main.ScreenToWorldPoint(Input.mousePosition)) - Weapon.transform.position;
+
+        this.transform.SetParent(Weapon.transform);
+        this.transform.localPosition = new Vector2(0f, 0f);
+
+        RB.velocity = bulletSpeed * (reticlePosRelativeToHilt).normalized;
+        this.transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Rad2Deg * Mathf.Atan2(reticlePosRelativeToHilt.y, reticlePosRelativeToHilt.x));
+
+        this.transform.SetParent(null);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+
+    public static Vector3 getMouseWorldPosition()
+    {
+        Camera worldCamera = Camera.main;
+        Vector3 vec = worldCamera.ScreenToWorldPoint(Input.mousePosition);
+
+        vec.z = 0f;
+        return vec;
+    }
 
 }
