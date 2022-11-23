17d9f2e1d1f7f98e6c507e50d1e692841c0ad144
kjtkanaly <kjtkanaly@gmail.com>
2022-11-23 14:23:34 -0600

    Progress on player 2d movement. Gonna add friction now

--- Assets/Player/Scripts/PlayerControllerTwo.cs
+++ Assets/Player/Scripts/PlayerControllerTwo.cs
@@ -6,17 +6,25 @@ public class PlayerControllerTwo : MonoBehaviour
 {
     public Rigidbody2D RB;
     public Collider2D FeetCollider;
+    public Collider2D HeadCollider;
+    public Collider2D RightCollider;
+    public Collider2D LeftCollider;
 
     public float gravityConstant    = -10f;
     public float LevitationForce    = 150f;
+    public float walkingSpeed       = 10f;
     public float mass               = 10f;
     public float terminalVelocity   = 15f;
+    public float maxHorizontalSpeed = 10f;
 
     public int LevitationMana       = 100;
 
-    public Vector2 PlayerVelocity = new Vector2(0f,0f);
+    public Vector2 PlayerVelocity   = new Vector2(0f,0f);
 
-    public bool AddGravity = true;
+    public bool AddGravity              = true;
+    public bool isHittingHead           = false;
+    public bool isRunningLeftIntoWall   = false;
+    public bool isRunningRightIntoWall  = false;
 
     // Update is called once per frame
     void Update()
@@ -30,11 +38,50 @@ public class PlayerControllerTwo : MonoBehaviour
         // Check if player jumped
         if ((Input.GetKey(KeyCode.Space)) && (LevitationMana > 0))
         {
-            PlayerVelocity += new Vector2(0f, (LevitationForce/mass) * Time.deltaTime);
+            if (isHittingHead == false)
+            {
+                PlayerVelocity += new Vector2(0f, (LevitationForce/mass) * Time.deltaTime);
+            }
+            else
+            {
+                PlayerVelocity = new Vector2(PlayerVelocity.x, 0f);
+            }
+        }
+
+        // Check for left/right movement
+        if ((Input.GetKey(KeyCode.D)) && !(Input.GetKey(KeyCode.A))) // Right
+        {
+            if (isRunningRightIntoWall == false)
+            {
+                PlayerVelocity = new Vector2(walkingSpeed, PlayerVelocity.y);
+                //PlayerVelocity += new Vector2(walkingSpeed * Time.deltaTime, 0f);
+            }
+            else
+            {
+                PlayerVelocity = new Vector2(0, PlayerVelocity.y);
+            }
+        }
+        else if (!(Input.GetKey(KeyCode.D)) && (Input.GetKey(KeyCode.A))) // Left
+        {
+            if (isRunningLeftIntoWall == false)
+            {
+                PlayerVelocity = new Vector2(-walkingSpeed, PlayerVelocity.y);
+                //PlayerVelocity -= new Vector2(walkingSpeed * Time.deltaTime, 0f);
+            }
+            else
+            {
+                PlayerVelocity = new Vector2(0, PlayerVelocity.y);
+            }
+        }
+        else if ((Input.GetKey(KeyCode.D)) && (Input.GetKey(KeyCode.A)))
+        {
+            PlayerVelocity = new Vector2(0, PlayerVelocity.y);
         }
         
         // Clamp Player Velocity
-        PlayerVelocity = Vector2.ClampMagnitude(PlayerVelocity, terminalVelocity);
+        //PlayerVelocity = Vector2.ClampMagnitude(PlayerVelocity, terminalVelocity);
+        PlayerVelocity.y = Mathf.Clamp(PlayerVelocity.y, -terminalVelocity, terminalVelocity);
+        PlayerVelocity.x = Mathf.Clamp(PlayerVelocity.x, -maxHorizontalSpeed, maxHorizontalSpeed);
 
         // Update RB
         RB.velocity = PlayerVelocity;
@@ -48,12 +95,54 @@ public class PlayerControllerTwo : MonoBehaviour
             AddGravity = false;
             PlayerVelocity = new Vector2(PlayerVelocity.x, 0f);
         }
+
+        if ((HeadCollider == col.otherCollider) || (HeadCollider == col.collider))
+        {
+            Debug.Log("Ouch Me Head");
+            isHittingHead = true;
+            PlayerVelocity = new Vector2(PlayerVelocity.x, 0f);
+        }
+
+        if ((RightCollider == col.otherCollider) || (LeftCollider == col.collider))
+        {
+            Debug.Log("Right Oof!");
+            isRunningRightIntoWall = true;
+            PlayerVelocity = new Vector2(0f, PlayerVelocity.y);
+        }
+
+        if ((LeftCollider == col.otherCollider) || (LeftCollider == col.collider))
+        {
+            Debug.Log("Left Oof!");
+            isRunningLeftIntoWall = true;
+            PlayerVelocity = new Vector2(0f, PlayerVelocity.y);
+        }
     }
 
     void OnCollisionExit2D(Collision2D col)
     {
-        Debug.Log("Liftoff");
-        AddGravity = true;
+        if ((FeetCollider == col.otherCollider) || (FeetCollider == col.collider))
+        {
+            Debug.Log("Liftoff");
+            AddGravity = true;
+        }
+
+        if ((HeadCollider == col.otherCollider) || (HeadCollider == col.collider))
+        {
+            Debug.Log("Phew");
+            isHittingHead = false;
+        }
+
+        if ((RightCollider == col.otherCollider) || (LeftCollider == col.collider))
+        {
+            Debug.Log("Right Wee!");
+            isRunningRightIntoWall = false;
+        }
+
+        if ((LeftCollider == col.otherCollider) || (LeftCollider == col.collider))
+        {
+            Debug.Log("Left Wee!");
+            isRunningLeftIntoWall = false;
+        }
     }
 
 }
