fda9da5756b087e5e0292cb9269f1b738fb60e21
kjtkanaly <kjtkanaly@gmail.com>
2022-11-23 13:15:38 -0600

    Levitation working

--- Assets/Player/Scripts/PlayerControllerTwo.cs
+++ Assets/Player/Scripts/PlayerControllerTwo.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerControllerTwo : MonoBehaviour
 {
     public Rigidbody2D RB;
+    public Collider2D FeetCollider;
 
     public float gravityConstant    = -10f;
+    public float LevitationForce    = 150f;
     public float mass               = 10f;
-    public float terminalVelocity   = 100f;
+    public float terminalVelocity   = 15f;
+
+    public int LevitationMana       = 100;
 
     public Vector2 PlayerVelocity = new Vector2(0f,0f);
 
@@ -22,6 +26,12 @@ public class PlayerControllerTwo : MonoBehaviour
         {
             PlayerVelocity += new Vector2(0f, gravityConstant * Time.deltaTime);
         }
+
+        // Check if player jumped
+        if ((Input.GetKey(KeyCode.Space)) && (LevitationMana > 0))
+        {
+            PlayerVelocity += new Vector2(0f, (LevitationForce/mass) * Time.deltaTime);
+        }
         
         // Clamp Player Velocity
         PlayerVelocity = Vector2.ClampMagnitude(PlayerVelocity, terminalVelocity);
@@ -32,9 +42,12 @@ public class PlayerControllerTwo : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        Debug.Log("Touchdown!");
-        AddGravity = false;
-        PlayerVelocity = new Vector2(PlayerVelocity.x, 0f);
+        if ((FeetCollider == col.otherCollider) || (FeetCollider == col.collider))
+        {
+            Debug.Log("Touchdown!");
+            AddGravity = false;
+            PlayerVelocity = new Vector2(PlayerVelocity.x, 0f);
+        }
     }
 
     void OnCollisionExit2D(Collision2D col)
@@ -42,4 +55,5 @@ public class PlayerControllerTwo : MonoBehaviour
         Debug.Log("Liftoff");
         AddGravity = true;
     }
+
 }
