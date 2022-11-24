ae8e389d379346ab7ad399692750c6ef4f1d6ee5
kjtkanaly <kjtkanaly@gmail.com>
2022-11-23 21:48:53 -0600

    Got player collisions fixed

--- Assets/Falling Sand Stuff/Spells/GenericProjectileSpell.cs
+++ Assets/Falling Sand Stuff/Spells/GenericProjectileSpell.cs
@@ -6,24 +6,37 @@ public class GenericProjectileSpell : Spell
 {
     // RB: Object's RigidBody
     // Trans: Object's Transform
-    public Rigidbody2D    RB;
-    public Transform      Trans;
-    public SpriteRenderer Spr;
-
-    public void Start()
-    {
-        //Physics.IgnoreCollision();
-    }
+    public Rigidbody2D      RB;
+    public Transform        Trans;
+    public SpriteRenderer   Spr;
+    public CircleCollider2D Col;
+    public BoxCollider2D[]  PlayerColliders;
 
     public void Awake()
     {
         RB      = this.GetComponent<Rigidbody2D>();
         Trans   = this.GetComponent<Transform>();        
         Spr     = this.GetComponentInChildren(typeof(SpriteRenderer), true) as SpriteRenderer;
+        Col     = this.GetComponent<CircleCollider2D>();
+        PlayerColliders = GameObject.FindGameObjectWithTag("Player").GetComponents<BoxCollider2D>();
     }
 
-    public void OnCollisionEnter2D()
+    public void OnEnable()
     {
+        Debug.Log(PlayerColliders.Length);
+        foreach (BoxCollider2D playerCol in PlayerColliders)
+        {
+            Physics2D.IgnoreCollision(Col, playerCol);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        /* if (col.collider.gameObject.tag == "Player")
+        {
+            Physics2D.IgnoreCollision(col.collider, col.otherCollider);
+        } */
+
         if (CanBounce == false)
         {
             this.gameObject.SetActive(false);
