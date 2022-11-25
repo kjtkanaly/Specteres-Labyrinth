51123222195d70e2a94421645f72f509655c29cb
kjtkanaly <kjtkanaly@gmail.com>
2022-11-25 14:35:14 -0600

    Wand Orientation fix

--- Assets/Falling Sand Stuff/Controller/WandOrientation.cs
+++ Assets/Falling Sand Stuff/Controller/WandOrientation.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class WandOrientation : MonoBehaviour
 {
     public Transform WandTrans;
+    public Transform PlayerTrans;
     public Vector2 MousePos;
 
     private void Start()
     {
         WandTrans = this.transform;
+        PlayerTrans = GameObject.FindWithTag("Player").transform;
     }
 
     private void Update()
     {
-        MousePos = new Vector2(Input.mousePosition.x - Screen.width/2, Input.mousePosition.y - Screen.height/2);
+        MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - PlayerTrans.position;
 
-        WandTrans.rotation = Quaternion.Euler(new Vector3(WandTrans.rotation.x, WandTrans.rotation.y, Mathf.Rad2Deg * Mathf.Atan2(MousePos.y, MousePos.x)));
+        WandTrans.rotation = Quaternion.Euler(new Vector3(WandTrans.rotation.x, WandTrans.rotation.y, 
+                            Mathf.Rad2Deg * Mathf.Atan2(MousePos.y, MousePos.x)));
     }
 }
